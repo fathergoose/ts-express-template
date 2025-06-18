32627f163c550f9d532aedd26966907fa8575a45
Alexander Ilseman <alexander.ilseman.com>
2025-06-17 19:52:02 -0500

    First pass at schema-WIP

--- db/migrations/20250616232652_initial_schema.sql
+++ db/migrations/20250616232652_initial_schema.sql
@@ -1,5 +1,15 @@
 -- migrate:up
-
-
+CREATE TABLE artists (id SERIAL PRIMARY KEY, name TEXT NOT NULL)
+CREATE TABLE generes (id SERIAL PRIMARY KEY, name TEXT NOT NULL)
+CREATE TABLE albums (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  album_artist_id INTEGER REFERENCES artists (id),
+  genere_id INTEGER REFERENCES generes (id)
+)
+CREATE TABLE tracks (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  artist_id
+)
 -- migrate:down
-
